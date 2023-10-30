3ef741ae40c8f3ba7547b321828d7728643755cc
Haruna Abe <114179729+nanoha94@users.noreply.github.com>
2023-10-30 16:01:47 +0000

    Delete src/fireabase/config.ts

--- src/fireabase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBb87bOraugmu1N2LOmXOdUba4JxKMrFwk",
-  authDomain: "simple-todo-71e1f.firebaseapp.com",
-  projectId: "simple-todo-71e1f",
-  storageBucket: "simple-todo-71e1f.appspot.com",
-  messagingSenderId: "792947718744",
-  appId: "1:792947718744:web:49ea63a2788735a87c282d",
-};
