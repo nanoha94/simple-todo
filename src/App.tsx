0f1cff47f49248a4d1ddfeb9fd8e93824fd3290a
nanoha <nanoha.creator@gmail.com>
2023-11-13 13:04:55 +0000

    スタイル調整

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import Header from "./components/Header";
 import Router from "./Router";
 
 const Main = styled.main`
-  width: calc(100% - 40px);
+  max-width: 760px;
+  width: 100%;
+  margin: 0 auto;
   padding: 20px;
   display: flex;
   flex-direction: column;
